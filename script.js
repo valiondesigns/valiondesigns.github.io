724743d9d60b915af20d227c857009ec5fb387e7
valiondesigns <valiondsigns@gmail.com>
2026-05-24 09:17:14 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            entry.target.classList.add("show");
-        }
-    });
-});
-
-document.querySelectorAll('.hidden').forEach(el => observer.observe(el));
